612c4f76f9d562b91818640aeec6cf595cced8a0
brooksburkhead <143526897+brooksburkhead@users.noreply.github.com>
2023-11-02 10:17:05 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,6 +8,8 @@ def get_metadata(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
   metadata_df['Null %'] = metadata_df['Nulls'] / df.shape[0] * 100
   metadata_df['Data Types'] = df.dtypes
   metadata_df['Memory'] = df.memory_usage(deep = True)
+  metadata_df['Unique'] = df.nunique()
   metadata_df = pd.concat([metadata_df, df.describe(include = 'all').transpose()], axis = 'columns')
+  
 
   return metadata_df
