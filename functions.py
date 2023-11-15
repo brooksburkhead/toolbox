1fe61f1561c3d2f47c066ab26847d017a5e4377e
Robert Citek <robert.citek@gmail.com>
2023-11-15 10:37:19 -0700

    Update functions.py
    Added id_columns_identify and made some cosmetic changes to get_metadata()

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 def get_metadata(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
-  ''' Given a dataframe, returns new dataframe containing metadata for given dataframe.
-
+  '''
+  Given a dataframe, returns new dataframe containing metadata for given dataframe.
   '''
   metadata_df = pd.DataFrame()
   metadata_df['Data Types'] = df.dtypes
@@ -11,6 +11,16 @@ def get_metadata(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
   metadata_df['Null %'] = metadata_df['Nulls'] / df.shape[0] * 100
   metadata_df = pd.concat([metadata_df, df.describe(include = 'all').transpose().astype({"count" : "int"})], axis = 'columns').rename(columns = {"top" : "mode", "50%" : "median"})
   metadata_df['unique'] = df.nunique()
-  
-
   return metadata_df
+
+def id_columns_identify( df, threshold = 90 ):
+  '''
+  Identifies columns that could be IDs. Returns a data frame for review.
+  '''
+  
+  md = get_metadata( df )
+  filter = ( md["unique"] >= threshold/100 * md["count"] )
+  cols_to_del = md[ filter ][["unique","count"]].index
+  return df[ cols_to_del ].head()
+
+
